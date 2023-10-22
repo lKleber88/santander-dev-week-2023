b14c40ff67f0bdfe6edf63b5aa766a04df80a590
Luiz Kleber <kalebst330@gmail.com>
2023-10-22 16:13:52 -0300

    Initial commit

---
+++ src/test/java/projetoDio/SantanderDevWeek2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package projetoDio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeek2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
